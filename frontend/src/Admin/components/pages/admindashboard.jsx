14789fdafc4c05f8ca854c272f876cb136ee8906
lava-neesh <lavaneeshgowda@gmail.com>
2026-04-28 10:49:46 +0530

    fix build error

--- frontend/src/Admin/components/pages/admindashboard.jsx
+++ frontend/src/Admin/components/pages/admindashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./AdminDashboard.css";
+import "./admindashboard.css";
 import { useNavigate } from "react-router-dom";
 import {
   LineChart,
